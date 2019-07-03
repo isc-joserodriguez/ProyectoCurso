c4a30e7b53768aa5be4e0b882a0a94e2d0e710a3
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-07-03 15:11:32 -0600

    Tokens

--- Endpoints/controllers/persona.controller.js
+++ Endpoints/controllers/persona.controller.js
@@ -1,25 +1,27 @@
 let _persona;
+const hash = require('../middlewares/password');
+const tkn = require('../middlewares/token');
 
-const getAll = (req,res)=>{
+const getAll = (req, res) => {
     _persona.find({})
-    .then(personas=>{
-        res.status(200);
-        res.json({
-            code:200,
-            msg:"Consulta exitosa.",
-            detail:personas
-        });        
-    }).catch(error=>{
+        .then(personas => {
+            res.status(200);
+            res.json({
+                code: 200,
+                msg: "Consulta exitosa.",
+                detail: personas
+            });
+        }).catch(error => {
             res.status(400);
             res.json({
-                code:400,
-                msg:"Error.",
-                detail:error
+                code: 400,
+                msg: "Error.",
+                detail: error
             });
-    });
+        });
 };
 
-const create = (req,res)=>{
+const create = (req, res) => {
     /*
     {
         "credencial":{
@@ -38,95 +40,170 @@ const create = (req,res)=>{
         "cursoAlumno":[{"_id":"id2"}]
     }*/
     const persona = req.body;
-    _persona.create(persona)
-    .then(data=>{
-        res.status(200);
-        res.json({
-            code:200,msg:"Guardado.",
-            detail:data
-        });
-    }).catch(error => {
-        res.status(400);
-        res.json({
+    _persona.find({}).sort({ _id: -1 }).then(regs => {
+        persona._id = parseInt((regs.length == 0) ? 0 : (regs[0].id)) + 1;
+        if (!persona.credencial.correo || !persona.credencial.contraseña) {
+            res.status(400).json({
+                code: 400,
+                msg: "Debe de ingresar correo y contraseña para registrarse.",
+                detail: []
+            });
+        } else if (regs.filter(u => u.credencial.correo === persona.credencial.correo).length > 0) {
+            res.status(400).json({
+                code: 400,
+                msg: "Ya existe ese correo.",
+                detail: []
+            });
+        } else {
+            hash.hashPassword(req.body.credencial.contraseña).then(contraseña => {
+                persona.credencial.contraseña = contraseña;
+                _persona.create(persona)
+                    .then(data => {
+                        res.status(200).json({
+                            code: 200, msg: "Saved!!!",
+                            detail: data
+                        });
+                    }).catch(error => {
+                        res.status(400).json({
+                            code: 400,
+                            msg: "No se pudo insertar!!!",
+                            detail: error
+                        });
+                    });
+            });
+        }
+    });
+}
+
+const login = (req, res) => {
+    correo = req.body.credencial.correo;
+    contraseña = req.body.credencial.contraseña;
+    _persona.find({})
+        .then(personas => {
+            usuario = personas.filter(u => (u.credencial.correo == correo))[0];
+            if (!usuario) {
+                res.status(400).json({
+                    code: 400,
+                    msg: "Error: El correo es incorrecto",
+                    data: []
+                });
+            } else {
+                hash.comparePassword(contraseña, usuario.credencial.contraseña).then(contraseñaCorrecta => {
+                    if (!contraseñaCorrecta) {
+                        res.status(400).json({
+                            code: 400,
+                            msg: "Error: La contraseña es incorrecta",
+                            data: []
+                        });
+                    } else {
+                        const tokenTTL = `${1000 * 60 * 60 * 24 * 30}ms`; // ms * s * m * h * d
+                        const payload = {
+                            id: usuario._id,
+                            tipo: usuario.tipo
+                        };
+                        const token = tkn.generateJWT(payload, tokenTTL);
+                        res.status(200).json({
+                            code: 200,
+                            msg: "Consulta exitosa.",
+                            detail: token
+                        });
+
+                    }
+
+                })
+            }
+        }).catch(error => {
+        res.status(400).json({
             code: 400,
-            msg: "Error al insertar.",
-            detail:error
+            msg: "No se pudo insertar!!!",
+            detail: error
         });
     });
 }
 
-const deletePersona = (req,res) =>{
-    const {id}= req.params;
-    _persona.remove({_id:id})
-    .then(data=>{
-        res.status(200);
-        res.json({
-            code:200,
-            msg:"Eliminado.",
-            detail:data
-        });
-    }).catch(error=>{
-        res.status(400);
-        res.json({
-            code:400,
-            msg:"Error al eliminar.",
-            detail:error
-        });
-    });
+const info = (req, res) => {
+    res.status(200).json({
+        code: 200,
+        msg: "Entraste correctamente!",
+        data: req.decoded
+    })
+
 }
 
-const getById = (req,res)=>{
-    const id= req.params.id;
-    _persona.find({_id:id})
-    .then(persona=>{
-        res.status(200);
-        res.json({
-            code:200,
-            msg:"Consulta exitosa.",
-            detail:persona
+const deletePersona = (req, res) => {
+    const { id } = req.params;
+    _persona.remove({ _id: id })
+        .then(data => {
+            res.status(200);
+            res.json({
+                code: 200,
+                msg: "Eliminado.",
+                detail: data
+            });
+        }).catch(error => {
+            res.status(400);
+            res.json({
+                code: 400,
+                msg: "Error al eliminar.",
+                detail: error
+            });
         });
-    }).catch(error=>{
-        res.status(400);
-        res.json({
-            code:400,
-            msg:"Error.",
-            detail:error
-        });
-    });
 }
 
-const update = (req,res)=>{
-    const {id}= req.params;
-    const persona=req.body;
-    _persona.update({_id:id},{$set:{
-        nombre:persona.nombre,
-        apPaterno:persona.apPaterno,
-        apMaterno:persona.apMaterno,
-        fechaNac:persona.fechaNac,
-        foto:persona.foto,
-        paginaWeb:persona.paginaWeb,
-    }})
-    .then(data=>{
-        res.status(200);
-        res.json({
-            code:200,
-            mgs:"Se editó con éxito",
-            detail:data
+const getById = (req, res) => {
+    const id = req.params.id;
+    _persona.find({ _id: id })
+        .then(persona => {
+            res.status(200);
+            res.json({
+                code: 200,
+                msg: "Consulta exitosa.",
+                detail: persona
+            });
+        }).catch(error => {
+            res.status(400);
+            res.json({
+                code: 400,
+                msg: "Error.",
+                detail: error
+            });
         });
-        
-    }).catch(error=>{
-        res.status(400);
-        res.json({
-            code:400,
-            msg:"Error.",
-            detail:error
-        });
-    });
 }
 
-module.exports = (Persona)=>{
+const update = (req, res) => {
+    const { id } = req.params;
+    const persona = req.body;
+    _persona.update({ _id: id }, {
+        $set: {
+            nombre: persona.nombre,
+            apPaterno: persona.apPaterno,
+            apMaterno: persona.apMaterno,
+            fechaNac: persona.fechaNac,
+            foto: persona.foto,
+            paginaWeb: persona.paginaWeb,
+        }
+    })
+        .then(data => {
+            res.status(200);
+            res.json({
+                code: 200,
+                mgs: "Se editó con éxito",
+                detail: data
+            });
+
+        }).catch(error => {
+            res.status(400);
+            res.json({
+                code: 400,
+                msg: "Error.",
+                detail: error
+            });
+        });
+}
+
+module.exports = (Persona) => {
     _persona = Persona;
     return ({
-        getAll, create, deletePersona, getById,update
+        getAll, create, deletePersona, getById, update, info, login
     });
 }
--- Endpoints/models/models.js
+++ Endpoints/models/models.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose');
-
+const config = require('../config');
 //recorrer objetos y arreglos facil
 const _ = require('underscore');
 
 module.exports = (wagner) => {
     //Conexion
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost:27017/PlataformaCursos',{useNewUrlParser:true});
+    mongoose.connect(config.db, { useNewUrlParser: true });
     //funcion callback que se ejecuta cuando el wagner pone disponible esa variable, pongo como valor mongoose
     //si quiero acceder a mi bd desde otro punto es acceder con wagner
-    wagner.factory('db',()=> mongoose);
-    
+    wagner.factory('db', () => mongoose);
+
     //Declarar modelos
     const Persona = require('./persona.model');
     const Curso = require('./curso.model');
     const Valoracion = require('./valoracion.model');
     const Comunidad = require('./comunidad.model');
-    const Cobro= require('./cobro.model');
-    const Codigo= require('./codigo.model');
-    const Compra= require('./compra.model');
+    const Cobro = require('./cobro.model');
+    const Codigo = require('./codigo.model');
+    const Compra = require('./compra.model');
 
-    
+
     const models = {
         Persona,
         Curso,
@@ -31,7 +31,7 @@ module.exports = (wagner) => {
         Compra
     }
     //recorrer los modelos y ponerlos disponibles
-    _.each(models,(v,k)=>{
-        wagner.factory(k,()=>v);
+    _.each(models, (v, k) => {
+        wagner.factory(k, () => v);
     });
 }
