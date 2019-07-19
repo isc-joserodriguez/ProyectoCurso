a4be23f280899e212174e41051ecb702155ce9d3
Carla Reyes <carla.cutte@hotmail.com>
2019-07-19 16:02:53 -0600

    home admin

--- royal-project/src/app/modulos/admin/admin-home/admin-home.module.ts
+++ royal-project/src/app/modulos/admin/admin-home/admin-home.module.ts
@@ -5,11 +5,18 @@ import { AdminHomeRoutingModule } from './admin-home-routing.module';
 import { AdminHomeComponent } from './admin-home.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
+//MDBootstrap
+import { CarouselModule } from 'angular-bootstrap-md';
+
+import { CardsFreeModule  } from 'angular-bootstrap-md';
+
 @NgModule({
   declarations: [AdminHomeComponent, UsuariosComponent],
   imports: [
     CommonModule,
-    AdminHomeRoutingModule
+    AdminHomeRoutingModule,
+    CarouselModule,
+    CardsFreeModule
   ]
 })
 export class AdminHomeModule { }
--- royal-project/src/app/modulos/maestro/maestro-home/maestro-home.module.ts
+++ royal-project/src/app/modulos/maestro/maestro-home/maestro-home.module.ts
@@ -4,11 +4,19 @@ import { CommonModule } from '@angular/common';
 import { MaestroHomeRoutingModule } from './maestro-home-routing.module';
 import { MaestroHomeComponent } from './maestro-home.component';
 
+//MDBootstrap
+import { CarouselModule } from 'angular-bootstrap-md';
+
+import { CardsFreeModule  } from 'angular-bootstrap-md';
+
+
 @NgModule({
   declarations: [MaestroHomeComponent],
   imports: [
     CommonModule,
-    MaestroHomeRoutingModule
+    MaestroHomeRoutingModule,
+    CarouselModule,
+    CardsFreeModule
   ]
 })
 export class MaestroHomeModule { }
