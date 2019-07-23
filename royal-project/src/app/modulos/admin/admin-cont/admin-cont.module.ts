ae42f44c1103aa4fd9a20fdd30d455f02dd1cb74
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-07-22 23:17:20 -0600

    Tabla usuarios - Admin

--- royal-project/src/app/modulos/admin/admin-cont/admin-cont.module.ts
+++ royal-project/src/app/modulos/admin/admin-cont/admin-cont.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AdminContRoutingModule } from './admin-cont-routing.module';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 
+// Material
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+
 // MDBootstrap
 import { CarouselModule } from 'angular-bootstrap-md';
-import { CardsFreeModule  } from 'angular-bootstrap-md';
+import { CardsFreeModule } from 'angular-bootstrap-md';
 import { AdminCursosComponent } from './admin-cursos/admin-cursos.component';
 import { AdminUsuariosComponent } from './admin-usuarios/admin-usuarios.component';
 import { AdminPagosComponent } from './admin-pagos/admin-pagos.component';
@@ -14,12 +20,22 @@ import { AdminEstadisticasComponent } from './admin-estadisticas/admin-estadisti
 import { AdminUsuarioNuevoComponent } from './admin-usuario-nuevo/admin-usuario-nuevo.component';
 
 @NgModule({
-  declarations: [AdminHomeComponent, AdminCursosComponent, AdminUsuariosComponent, AdminPagosComponent, AdminEstadisticasComponent, AdminUsuarioNuevoComponent],
+  declarations: [
+    AdminHomeComponent,
+    AdminCursosComponent,
+    AdminUsuariosComponent,
+    AdminPagosComponent,
+    AdminEstadisticasComponent,
+    AdminUsuarioNuevoComponent
+  ],
   imports: [
     CommonModule,
     AdminContRoutingModule,
     CarouselModule,
-    CardsFreeModule
+    CardsFreeModule,
+    MatTableModule,
+    MatInputModule,
+    MatPaginatorModule
   ]
 })
 export class AdminContModule { }
--- royal-project/src/app/modulos/admin/admin-cont/admin-usuarios/admin-usuarios.component.ts
+++ royal-project/src/app/modulos/admin/admin-cont/admin-usuarios/admin-usuarios.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-admin-usuarios',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-usuarios.component.scss']
 })
 export class AdminUsuariosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  displayedColumns: string[] = ['img', 'nombre', 'apellidos', 'estatus', 'acciones'];
+  dataSource: MatTableDataSource<any>;
+  
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+  constructor() {
+    this.dataSource = new MatTableDataSource([
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Alfonso', apellidos: 'Lopez Meza', estatus: 'Activo', acciones: 'hola' },
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Mario', apellidos: 'Luna Gonzalez', estatus: 'Activo', acciones: 'hola' },
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Laura', apellidos: 'Martinez Anaya', estatus: 'Activo', acciones: 'hola' },
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Samuel', apellidos: 'Ruiz Luna', estatus: 'Activo', acciones: 'hola' },
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Carlos', apellidos: 'Reyes Lopez', estatus: 'Activo', acciones: 'hola' },
+      { img: 'http://www.lorempixel.com/200/200', nombre: 'Pablo', apellidos: 'Hola Adios', estatus: 'Activo', acciones: 'hola' }
+    ]);
   }
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
