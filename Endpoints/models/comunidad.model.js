68876312886d215244b6573147c8c10c4d6e8302
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-10-16 22:03:51 -0600

    Maestro - CursoEstatus

--- Endpoints/models/comunidad.model.js
+++ Endpoints/models/comunidad.model.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose');
 //esquema
 //definir el esquema que vamos a manejar
 const comunidadSchema = new mongoose.Schema({
-    _id:{type:String, required:true},
-    idPersona:{type: String, required:true},
-    nombreTema:{type:String,required:true},
-    cuerpoTema:{type:String, required:true},
-    fechaTema:{type:Date, required:true},
-    categoriaTema:{type:Number,required:true},
-    repuestas:[{_id:{type:String, required:true},idPersona:{type: String, required:true},comentario:{type: String, required:true},fecha:{type:Date,required:true}}]
+    _id: { type: Number, required: true },
+    idPersona: { type: Number, required: true },
+    nombreTema: { type: String, required: true },
+    cuerpoTema: { type: String, required: true },
+    fechaTema: { type: Date, required: true },
+    categoriaTema: { type: Number, required: true }, //0 - Tecnología 1 - Idiomas
+    repuestas: [{ _id: { type: Number }, idPersona: { type: Number }, comentario: { type: String }, fecha: { type: Date } }]
 });
 
 //modelo Curso
-const comunidadModel = mongoose.model('Comunidad', comunidadSchema,'comunidades');
+const comunidadModel = mongoose.model('Comunidad', comunidadSchema, 'comunidades');
 
 //exportar
-module.exports= comunidadModel;
+module.exports = comunidadModel;
---
+++ royal-project/src/app/modulos/maestro/perfil-maestro/perfil-maestro-cont/estatus-cursos/estatus-cursos.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CursosService } from '../../../../../servicios/cursos.service';
+
+@Component({
+  selector: 'app-estatus-cursos',
+  templateUrl: './estatus-cursos.component.html',
+  styleUrls: ['./estatus-cursos.component.scss']
+})
+export class EstatusCursosComponent implements OnInit {
+  respuesta: any = {
+    code: 0,
+    msg: '',
+    detail: ''
+  };
+
+  // Variables Cursos
+  listaCursos = [];
+  colCursos: string[] = ['curso', 'fecha', 'estado', 'publicado', 'editar'];
+  datosCursos: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private router:Router, private cursos: CursosService) {
+    // datasource cursos
+    this.datosCursos = new MatTableDataSource(this.listaCursos);
+  }
+
+  ngOnInit() {
+    // inic. cursos
+    this.datosCursos.paginator = this.paginator;
+    this.datosCursos.sort = this.sort;
+    this.getCursos();
+  }
+
+  getCursos() {
+    this.listaCursos = [];
+    this.cursos.getCursos().subscribe(res => {
+      this.respuesta = res;
+      this.respuesta.detail.forEach(curso => {
+        console.log(curso)
+        if (curso.idMaestro == localStorage.getItem('userid')) {
+          this.listaCursos.push({
+            id: curso._id,
+            curso: curso.nombreCorto,
+            fecha: curso.fechaSolicitud,
+            ruta: curso.ruta,
+            estado: this.getEstatus(curso.estado),
+            publicado: curso.publicacion
+          });
+          this.datosCursos = new MatTableDataSource(this.listaCursos);
+          this.datosCursos.paginator = this.paginator;
+          this.datosCursos.sort = this.sort;
+        }
+      });
+    });
+  }
+  getEstatus(estado) {
+    if (estado == 1) {
+      return 'En revisión';
+    } else if (estado == 2) {
+      return 'Aceptado';
+    } else {
+      return 'Rechazado';
+    }
+  }
+  configCurso(ruta){
+    this.router.navigate(['/maestro/curso/config/',ruta]);
+  }
+}
