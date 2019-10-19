999b754fc77adb0a28c72497ce35657c9873cce4
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-10-18 21:45:24 -0600

    Model - Persona

--- Endpoints/models/persona.model.js
+++ Endpoints/models/persona.model.js
@@ -14,7 +14,7 @@ const personaSchema = new mongoose.Schema({
     apPaterno: { type: String },
     apMaterno: { type: String },
     fechaNac: { type: Date, default: Date.now() },
-    foto: { type: String, default:'http://www.lorempixel.com/200/200' },
+    foto: { type: String, default: 'http://www.lorempixel.com/200/200' },
     web: { type: String, default: ' ' },
     fb: { type: String, default: ' ' },
     yt: { type: String, default: ' ' },
@@ -30,7 +30,8 @@ const personaSchema = new mongoose.Schema({
     identificacion: { type: String },
     curriculum: { type: String },
     notificaciones: [{ _id: { type: String, required: true }, personaId: { type: String, required: true }, fecha: { type: Date, required: true }, descripción: { type: String, required: true }, estado: { type: Boolean, required: true } }],
-    resumen: { type: String, default: ' '}
+    resumen: { type: String, default: ' ' },
+    puntaje: { type: Number, default: 0 }
 });
 
 //modelo Persona
