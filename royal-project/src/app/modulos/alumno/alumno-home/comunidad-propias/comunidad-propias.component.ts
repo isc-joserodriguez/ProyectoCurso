fb8c5fb6e9211a9840e95acfe2e9808de795d310
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2019-11-01 01:52:11 -0700

    Comunidad - Mis preguntas

--- royal-project/src/app/modulos/alumno/alumno-home/comunidad-propias/comunidad-propias.component.ts
+++ royal-project/src/app/modulos/alumno/alumno-home/comunidad-propias/comunidad-propias.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ComunidadService } from 'src/app/servicios/comunidad.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comunidad-propias',
@@ -7,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComunidadPropiasComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private comunidad: ComunidadService, private formBuilder: FormBuilder, private router: Router) { }
+  preguntas = [];
+  preguntaForm: FormGroup;
+  categoriaSelect = 'idiomas';
   ngOnInit() {
-    window.scrollTo(0, 0);
+    this.preguntaForm = this.formBuilder.group({
+      pregunta: ['', Validators.required]
+    });
+    if (localStorage.getItem('pregunta') != null) {
+      this.preguntaForm.setValue({
+        pregunta: localStorage.getItem('pregunta')
+      });
+    }
+    this.getPreguntas();
+  }
+  getPreguntas() {
+    this.comunidad.getPreguntas().subscribe((preguntas: any) => {
+      preguntas.detail.forEach(pregunta => {
+        if (pregunta.idPersona == localStorage.getItem('userid')) {
+          this.preguntas.push(pregunta);
+        }
+      });;
+    });
+  }
+
+  hacerPregunta() {
+    localStorage.setItem('pregunta', this.preguntaForm.value.pregunta);
+    this.router.navigate(['/comunidad/nueva', this.categoriaSelect]);
   }
 
 }
